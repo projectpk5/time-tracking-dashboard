da471d5bc882fa2155c85dca30ba18a88467940e
projectpk5 <npkin567@yahoo.com>
2025-08-07 16:12:54 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function loadData() {
+  try {
+    const myData = require('./data.json');
+  } catch (error) {
+    console.error("error occured:",error);
+  }
+}
+
+
+
+
+function clickDaily() {
+
+}
+
